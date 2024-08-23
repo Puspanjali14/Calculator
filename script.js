6c2fdb42171d9cb30bb14b578ca48606d6f39b83
Puspanjali Neupane <neupanepuspanjali@gmail.com>
2024-08-23 21:21:21 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
